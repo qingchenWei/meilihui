d40600131261c7f470b5ab03de77177223d900b5
957631042 <957631042@qq.com>
2020-03-03 23:33:52 +0800

    卫鹏辉branch

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   lintOnSave: false,
   devServer: {
-    proxy: {
-      // '/ajax': {
-      //     target: 'http://m.maoyan.com',
-      //     changeOrigin: true,
-    }
+    // proxy: {
+    //   // '/ajax': {
+    //   //     target: 'http://m.maoyan.com',
+    //   //     changeOrigin: true,
+    // }
   }
 }
